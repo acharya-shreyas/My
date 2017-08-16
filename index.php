6feb267d1b16d60d31eaa26883b84d65afbb66b5
acharya-shreyas <31078329+acharya-shreyas@users.noreply.github.com>
2017-08-17 02:40:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,4 @@
 <?php
-
-echo $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-echo "<br/><hr/>";
-echo $server = $url["host"];
-echo "<br/>";
-echo $username = $url["user"];
-echo "<br/>";
-echo $password = $url["pass"];
-echo "<br/>";
-echo $db = substr($url["path"], 1);
-echo "<br/>";
-
-die("shreyas");
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
